acf0b9c796f45841b5f076ed13dc1a7475641826
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-09-01 21:21:35 -0300

    adding dotenv

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
